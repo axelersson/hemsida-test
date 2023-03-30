0a1556feb717fc1d6799a4d59084b0c56fd4e809
axelersson <90241829+axelersson@users.noreply.github.com>
2023-03-30 12:02:50 +0200

    testat skapa mongodbdatabas

---
+++ server/mongoschemas/userSchema.js
@@ -0,0 +1,10 @@
+/* const mongoose = require("mongoose");
+
+const userModel = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  email: String,
+});
+
+module.exports = mongoose.model("User", userModel);
+ */
